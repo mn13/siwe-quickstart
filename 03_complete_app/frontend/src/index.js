f52df3d16b011805317feabd6fc8fd25afd2d566
Maks Nabokov <max.nab@equilibrium.io>
2024-08-24 20:02:46 +0300

    Revert "update siwe version and use scheme option (#41)"
    This reverts commit 9aa65712135c7aa57d9cbe16157fe1c5f64134c7.

--- 03_complete_app/frontend/src/index.js
+++ 03_complete_app/frontend/src/index.js
@@ -1,7 +1,6 @@
 import { BrowserProvider } from 'ethers';
 import { SiweMessage } from 'siwe';
 
-const scheme = window.location.protocol.slice(0, -1);
 const domain = window.location.host;
 const origin = window.location.origin;
 const provider = new BrowserProvider(window.ethereum);
@@ -12,7 +11,6 @@ async function createSiweMessage(address, statement) {
         credentials: 'include',
     });
     const message = new SiweMessage({
-        scheme,
         domain,
         address,
         statement,
